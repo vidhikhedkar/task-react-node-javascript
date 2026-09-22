2d8f0f9250f57db70c2115f223a5cd250d222ca2
vidhikhedkar <vidhikhedkar7@gmail.com>
2026-09-22 22:43:49 +0530

    add port

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,8 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/student_db
 mongoose.connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch(err => console.error('MongoDB connection error:', err));
